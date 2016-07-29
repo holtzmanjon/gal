5b1972185a3f3c95be9ee73cc90e0a3b6d142737
Jon Holtzman <holtz@nmsu.edu>
2016-07-29 14:22:03 -0600

    add stars

---
+++ stars.py
@@ -0,0 +1,12 @@
+def ghb(jk0,feh,dwarf=False) :
+    """
+    Color-temperature relation from Gonzalez Hernandez & Bonifacio (2009):  (J-K)_0 - Teff
+    """
+    if dwarf :
+      b0=0.6524 ; b1=0.5813 ; b2=0.1225 ; b3=-0.0646 ; b4=0.0370 ; b5=0.0016 # dwarfs
+    else :
+      b0=0.6517 ; b1=0.6312 ; b2=0.0168 ; b3=-0.0381 ; b4=0.0256 ; b5=0.0013 # giants
+    theta=b0+b1*jk0+b2*jk0**2+b3*jk0*feh+b4*feh+b5*feh**2
+
+    return 5040./theta
+
