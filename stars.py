5d2eb7dacf3bdf6453be37801e12ba33ccd9f653
Jon Holtzman <holtz@nmsu.edu>
2017-02-12 20:36:41 -0700

    updates

--- stars.py
+++ stars.py
@@ -7,6 +7,8 @@ def ghb(jk0,feh,dwarf=False) :
     else :
       b0=0.6517 ; b1=0.6312 ; b2=0.0168 ; b3=-0.0381 ; b4=0.0256 ; b5=0.0013 # giants
     theta=b0+b1*jk0+b2*jk0**2+b3*jk0*feh+b4*feh+b5*feh**2
+    dtheta_djk = b1+2*b2*jk0+b3*feh
+    dt_djk= -5040./theta**2*dtheta_djk
 
-    return 5040./theta
+    return 5040./theta, dt_djk
 
